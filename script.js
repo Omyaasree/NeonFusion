530eba18d57d1d9de4af4127491cc0644e94cb6e
Omyaasree Balaji <osbalaji@dons.usfca.edu>
2025-06-10 12:44:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const img = document.createElement('img');
+img.src = 'brochure.png'; // Make sure this matches your file name
+img.alt = 'Brochure';
+img.style.width = '100%';
+img.style.display = 'block';
+
+document.getElementById('container').appendChild(img);
