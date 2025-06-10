a52a17941717cab695447842a3b3a0a4eb565685
Omyaasree Balaji <osbalaji@dons.usfca.edu>
2025-06-10 13:10:53 +0530

    spell check

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const img = document.createElement('img');
-img.src = 'brochure.png'; // Make sure this matches your file name
+img.src = 'Brochure.png'; // Make sure this matches your file name
 img.alt = 'Brochure';
 img.style.width = '100%';
 img.style.display = 'block';
